e5264823f3052fce78a8d72b05d9cfbda0953872
Dogpa <dogpa0130@gmail.com>
2021-09-03 14:49:01 +0800

    Initial Commit

---
+++ Swift Practice # 69 ChooseQuestion/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Swift Practice # 69 ChooseQuestion
+//
+//  Created by Dogpa's MBAir M1 on 2021/9/3.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
